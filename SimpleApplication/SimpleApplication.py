2de5593ead2b10af51efd102a8bf776ed047e8c1
Danae <danaekoh@gmail.com>
2020-01-14 23:20:17 +0800

    view item page

--- SimpleApplication/SimpleApplication.py
+++ SimpleApplication/SimpleApplication.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 from forms import CreateUserForm
 from itemForm import CreateItemForm, serialcheck
 import shelve, User, itemclass, itemForm
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/cart')
 def cart():
@@ -16,35 +18,38 @@ def cart():
 
 @app.route('/contactUs')
 def contactUs():
- return render_template('contactUs.html')
+    return render_template('contactUs.html')
 
 
 @app.route('/staffHome')
 def staffHome():
     return render_template('staffHome.html')
 
+
 @app.route('/inventory')
 def inventory():
     return render_template('inventory.html')
 
+
 @app.route('/createUser', methods=['GET', 'POST'])
 def createUser():
- createUserForm = CreateUserForm(request.form)
+    createUserForm = CreateUserForm(request.form)
 
- if request.method == 'POST' and createUserForm.validate():
-    usersDict = {}
-    db = shelve.open('storage.db', 'c')
-    try:
-        usersDict = db['Users']
-    except:
-        print("Error in retrieving Users from storage.db.")
-        user = User.User(createUserForm.firstName.data,createUserForm.lastName.data, createUserForm.membership.data,createUserForm.gender.data, createUserForm.remarks.data)
-        usersDict[user.get_userID()] = user
-        db['Users'] = usersDict
-        db.close()
-        return redirect(url_for('retrieveUsers'))
-    return redirect(url_for('home'))
- return render_template('createUser.html', form=createUserForm)
+    if request.method == 'POST' and createUserForm.validate():
+        usersDict = {}
+        db = shelve.open('storage.db', 'c')
+        try:
+            usersDict = db['Users']
+        except:
+            print("Error in retrieving Users from storage.db.")
+            user = User.User(createUserForm.firstName.data, createUserForm.lastName.data,
+                             createUserForm.membership.data, createUserForm.gender.data, createUserForm.remarks.data)
+            usersDict[user.get_userID()] = user
+            db['Users'] = usersDict
+            db.close()
+            return redirect(url_for('retrieveUsers'))
+        return redirect(url_for('home'))
+    return render_template('createUser.html', form=createUserForm)
 
 
 @app.route('/retrieveUsers')
@@ -57,7 +62,8 @@ def retrieveUsers():
     for key in usersDict:
         user = usersDict.get(key)
         usersList.append(user)
-    return render_template('retrieveUsers.html',usersList=usersList, count=len(usersList))
+    return render_template('retrieveUsers.html', usersList=usersList, count=len(usersList))
+
 
 @app.route('/salesReports')
 def salesReports():
@@ -66,11 +72,22 @@ def salesReports():
 
 @app.route('/itempage')
 def itempage():
-    return render_template('itempage.html')
+    itemDict = {}
+    db = shelve.open('storage.db', 'r')
+    itemDict = db['Items']
+    db.close()
+
+    itemList = []
+    for key in itemDict:
+        item = itemDict.get(key)
+        itemList.append(item)
+    return render_template('itempage.html', itemList=itemList, count=len(itemList))
+
 
 @app.route('/viewItem')
 def viewItem():
     return render_template('itempage.html')
+
 
 @app.route('/itemCreation', methods=['GET', 'POST'])
 def itemCreation():
@@ -83,18 +100,21 @@ def itemCreation():
             itemsDict = db['Items']
         except:
             print("Error in retrieving Items from storage.db.")
-            item = itemclass.Item(createItemForm.itemName.data,createItemForm.itemCategory.data, createItemForm.itemGender.data,createItemForm.itemSerial.data)
+            item = itemclass.Item(createItemForm.itemName.data, createItemForm.itemCategory.data,
+                                  createItemForm.itemGender.data, createItemForm.itemSerial.data)
             itemsDict[item.get_itemSerial()] = item
             db['Items'] = itemsDict
             print(db['Items'])
             db.close()
-            return redirect(url_for('home'))
-        return redirect(url_for('home'))
+            return redirect(url_for('itempage'))
+        return redirect(url_for('itempage'))
     return render_template('itemCreation.html', form=createItemForm)
+
 
 @app.route('/createNewReport')
 def createNewReport():
     return render_template('create.html')
+
 
 if __name__ == '__main__':
     app.run()
