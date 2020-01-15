9af24215822cabb6713391bdc14ebc73396aacf5
Danae <danaekoh@gmail.com>
2020-01-16 00:52:55 +0800

    - fixed invoice creation form

--- SimpleApplication/invoiceForm.py
+++ SimpleApplication/invoiceForm.py
@@ -1,6 +1,8 @@
 from wtforms import Form, StringField, SelectField, validators, ValidationError, FloatField,DateField
+from invoiceclass import Invoice
 
 class CreateInvoiceForm(Form):
+    invoiceNumber = Invoice.invoiceN
     invoiceDate = DateField("Invoice Date ( d-m-Y )", [validators.DataRequired()],format='%d-%m-%Y')
     shipmentDate = DateField("Shipment Date ( d-m-Y )", [validators.DataRequired()],format='%d-%m-%Y')
     shipmentStatus = SelectField("Shipment Status", [validators.DataRequired()],choices=[('', 'Select'), ('Shipped', 'Shipped'), ('Received', 'Received')],default='')
