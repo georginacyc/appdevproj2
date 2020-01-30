e5a2916b4fc394c48cf5b576c482d32fb639708e
Danae <danaekoh@gmail.com>
2020-01-30 18:21:43 +0800

    update invoiceForm

--- SimpleApplication/invoiceForm.py
+++ SimpleApplication/invoiceForm.py
@@ -22,11 +22,10 @@ class CreateInvoiceForm(Form):
     invoiceNumber = Invoice.invoiceN
     invoiceDate = DateField("Invoice Date ( d-m-Y )", [validators.DataRequired()], format='%d-%m-%Y')
     shipmentDate = DateField("Shipment Date ( d-m-Y )", [validators.DataRequired()], format='%d-%m-%Y')
-    shipmentStatus = SelectField("Shipment Status", [validators.DataRequired()],choices=[('', 'Select'),('Ordered', 'Ordered'), ('Shipped', 'Shipped'), ('Received', 'Received')], default='')
-    receivedDate = DateField("Received Date ( d-m-Y )", [validators.DataRequired()], format='%d-%m-%Y')
     itemSerial = StringField("Item Serial", [validators.DataRequired(), Iserialcheck])
     orderQuantity = IntegerField("Order Quantity", [validators.DataRequired()])
 
 
 class UpdateInvoiceForm(Form):
-    pass
+    shipmentStatus = SelectField("Shipment Status", [validators.DataRequired()],choices=[('', 'Select'),('Ordered', 'Ordered'), ('Shipped', 'Shipped'), ('Received', 'Received')], default='')
+    receivedDate = DateField("Received Date ( d-m-Y )", [validators.DataRequired()], format='%d-%m-%Y')
