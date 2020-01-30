3d8aa73a9980c1b718da2b859a8356516e03c77e
sicholan <xlorex50@gmail.com>
2020-01-30 13:46:25 +0800

    cart class

--- SimpleApplication/Cart.py
+++ SimpleApplication/Cart.py
@@ -1,2 +1,29 @@
 class Cart:
-    def __init__(self):
+    countID = 0
+    def __init__(self,cartItem, cartQuantity, cartSubtotal):
+        self.__class__.countID += 1
+        self.__cartCount = self.__class__.countID
+        self.__cartItem = cartItem
+        self.__cartQuantity = cartQuantity
+        self.__cartSubtotal = cartSubtotal
+
+
+    def get_cartCount(self):
+        return self.__cartCount
+    def get_cartItem(self):
+        return self.__cartItem
+    def get_cartQuantity(self):
+        return self.__cartQuantity
+    def get_cartSubtotal(self):
+        return self.__cartSubtotal
+
+    def set_cartItem(self,cartItem):
+        self.__cartItem = cartItem
+    def set_cartQuantity(self,cartQuantity):
+        self.__cartQuantity = cartQuantity
+    def set_cartSubtotal(self,cartSubtotal):
+        self.__cartSubtotal = cartSubtotal
+
+    def set_updatePrice(self):
+
+
