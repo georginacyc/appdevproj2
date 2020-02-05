d57c748208b1f764f620f8b586657fec628e505e
Danae <danaekoh@gmail.com>
2020-02-05 14:35:25 +0800

    added some items and item description

--- SimpleApplication/Item.py
+++ SimpleApplication/Item.py
@@ -1,7 +1,8 @@
 class Item:
     countID = 0
-    def __init__(self,itemSerial,itemName,itemCategory,itemGender,itemCost,itemPrice,itemDescription):
-        self.__class__.countID +=1
+
+    def __init__(self, itemSerial, itemName, itemCategory, itemGender, itemCost, itemPrice, itemDescription):
+        self.__class__.countID += 1
         self.__itemCount = self.__class__.countID
         self.__itemName = itemName
         self.__itemSerial = itemSerial
@@ -13,50 +14,66 @@ class Item:
         self.__itemDescription = itemDescription
         self.set_itemStockStatus()
 
-    #get attributes
+    # get attributes
     def get_itemCount(self):
         return self.__itemCount
+
     def get_itemName(self):
         return self.__itemName
+
     def get_itemSerial(self):
         return self.__itemSerial
+
     def get_itemCategory(self):
         return self.__itemCategory
+
     def get_itemGender(self):
         return self.__itemGender
+
     def get_itemCost(self):
         return self.__itemCost
+
     def get_itemPrice(self):
         return self.__itemPrice
+
     def get_itemQuantity(self):
         return self.__itemQuantity
+
     def get_itemStockStatus(self):
         return self.__itemStockStatus
+
     def get_itemDescription(self):
         return self.__itemDescription
 
-    #set attributes
+    # set attributes
 
-    def set_itemSerial(self,itemSerial):
-        self.__itemSerial=itemSerial
-    def set_itemName(self,itemName):
+    def set_itemSerial(self, itemSerial):
+        self.__itemSerial = itemSerial
+
+    def set_itemName(self, itemName):
         self.__itemName = itemName
-    def set_itemCategory(self,itemCategory):
+
+    def set_itemCategory(self, itemCategory):
         self.__itemCategory = itemCategory
-    def set_itemGender(self,itemGender):
+
+    def set_itemGender(self, itemGender):
         self.__itemGender = itemGender
-    def set_itemCost(self,itemCost):
+
+    def set_itemCost(self, itemCost):
         self.__itemCost = itemCost
-    def set_itemPrice(self,itemPrice):
+
+    def set_itemPrice(self, itemPrice):
         self.__itemPrice = itemPrice
-    def set_itemQuantity(self,itemQuantity):
+
+    def set_itemQuantity(self, itemQuantity):
         self.__itemQuantity += itemQuantity
-    def set_itemDescription(self,itemDescription):
+
+    def set_itemDescription(self, itemDescription):
         self.__itemDescription = itemDescription
 
     def set_itemStockStatus(self):
         quantity = self.__itemQuantity
-        if quantity>30:
+        if quantity > 30:
             self.__itemStockStatus = "In Stock"
         elif 30 >= quantity > 0:
             self.__itemStockStatus = "Low on Stock"
