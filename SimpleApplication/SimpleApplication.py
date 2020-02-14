8bda4b1b036414ab04866bc08e3a5bbd66999a95
Nichoo <xlorex50@gmail.com>
2020-02-14 14:03:20 +0800

    invoice

--- SimpleApplication/SimpleApplication.py
+++ SimpleApplication/SimpleApplication.py
@@ -993,14 +993,14 @@ def retrieveNormalAnnouncements():  # to display announcements for normal staff
 @app.before_request
 def deleteDict():  # to use for deletion of any persistence
     dict = {}
-    #db = shelve.open("storage.db", "w")
-    #db["itemcount"] = dict
+    # db = shelve.open("storage.db", "w")
+    # db["Shipping"] = dict
     #db["Items"] = dict
     #db["StockOrder"] = dict
     #db["stockordercount"] = dict
     # # db["staffCount"] = dict
-    #db.close()
-    #print("Cleared")
+    # db.close()
+    # print("Cleared")
 
 
 @app.route('/deleteAnnouncement/<int:id>', methods=['GET', 'POST'])
